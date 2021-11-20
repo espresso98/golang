0b7a52b7e28ac50b290e24a6e5dd63b19117cc3b
Youra Cho <choyoura@gmail.com>
2021-11-19 21:21:46 -0800

    Add basic-types.go

---
+++ Foundation/basic-types.go
@@ -0,0 +1,26 @@
+/* basic types of go
+   bool string
+   int  int8  int16  int32(rune)  int64
+   uint uint8(byte) uint16 uint32 uint64 uintptr
+   float32 float64
+   complex64 complex128
+*/
+
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
