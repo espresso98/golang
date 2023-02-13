af3b486619a98ff55627afba48df6e3904f45dc3
Youra Cho <choyoura@gmail.com>
2023-02-12 22:48:11 -0800

    Add iota.go

---
+++ iota.go
@@ -0,0 +1,55 @@
+// Iota in Go is used to represent constant increasing sequences.
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	_  = 1 << (iota * 10) // ignore the first value
+	KB                    // decimal:       1024 -> binary 00000000000000000000010000000000
+	MB                    // decimal:    1048576 -> binary 00000000000100000000000000000000
+	GB                    // decimal: 1073741824 -> binary 01000000000000000000000000000000
+)
+
+func main() {
+	fmt.Println(KB, MB, GB)
+}
+
+// KB =  1024
+// MB =  1048576
+// GB =  1073741824
+
+// const (
+// 	January Month = 1 + iota
+// 	February
+// 	March
+// 	April
+// 	May
+// 	June
+// 	July
+// 	August
+// 	September
+// 	October
+// 	November
+// 	December
+// )
+
+// const (
+// 	Adventure Genre = iota + 1
+// 	Comic
+// 	Crime
+// 	Fiction
+// 	Fantasy
+// 	Historical
+// 	Horror
+// 	Magic
+// 	Mystery
+// 	Philosophical
+// 	Political
+// 	Romance
+// 	Science
+// 	Superhero
+// 	Thriller
+// 	Western
+// )
